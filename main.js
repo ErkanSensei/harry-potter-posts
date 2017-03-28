52d51e11973d6d4f68d01ecbcfd7515abc831f23
ErkanSensei <erkansen14@hotmail.com>
2017-03-28 17:31:07 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+import Expo from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Open up main.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+Expo.registerRootComponent(App);
